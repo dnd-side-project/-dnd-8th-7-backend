4c8e2b3c74ddc5fdf3b712f7c7dffd9468b81dff
pasly0920 <inwoo920621@gmail.com>
2023-01-20 11:07:36 +0900

    fix: directory 구조 변경

--- src/main/java/com/dnd8th/Dnd8th7Application.java
+++ src/main/java/com/dnd8th/Dnd8th7Application.java
@@ -1,4 +1,4 @@
-package com.example.dnd8th;
+package com.dnd8th;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Dnd8th7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Dnd8th7Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Dnd8th7Application.class, args);
+    }
 
 }
---
+++ src/test/java/com/dnd8th/Dnd8th7ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.dnd8th;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd8th7ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
