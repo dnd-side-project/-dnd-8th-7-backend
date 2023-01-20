7829661a85c42d4506435ceb0445b07a68ccb654
pasly0920 <inwoo920621@gmail.com>
2023-01-20 10:11:00 +0900

    chore: initial setup

---
+++ src/main/java/com/example/dnd8th/Dnd8th7Application.java
@@ -0,0 +1,13 @@
+package com.example.dnd8th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd8th7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dnd8th7Application.class, args);
+	}
+
+}
