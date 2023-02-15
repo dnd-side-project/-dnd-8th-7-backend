6d18bc1f4d03cf88ed974e60c67564800fbdd609
pasly0920 <inwoo920621@gmail.com>
2023-02-15 11:06:28 +0900

    feat: global error handler 추가

---
+++ src/main/java/com/dnd8th/error/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.dnd8th.error;
+
+import com.dnd8th.error.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+
+    private String message;
+    private int status;
+    private String code;
+
+    private ErrorResponse(final ErrorCode code) {
+        this.message = code.getMessage();
+        this.status = code.getStatus();
+        this.code = code.getCode();
+    }
+
+    public static ErrorResponse from(final ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+}
