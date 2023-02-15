12280120ad363659860dc4d545b0203684d7f90b
pasly0920 <inwoo920621@gmail.com>
2023-02-15 11:04:10 +0900

    feat: BaseEntity 추가 생성일, 수정일 추적

--- src/main/java/com/dnd8th/Dnd8th7Application.java
+++ src/main/java/com/dnd8th/Dnd8th7Application.java
@@ -2,7 +2,9 @@ package com.dnd8th;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Dnd8th7Application {
 
