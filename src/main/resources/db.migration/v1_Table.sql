df46a84af8415bbe3155799dae8e4d0d2ccf0d8e
rineeee <harin14@naver.com>
2023-02-18 00:33:59 +0900

    feat: add database sql

--- src/main/resources/db.migration/v1_Table.sql
+++ src/main/resources/db.migration/v1_Table.sql
@@ -1,6 +1,27 @@
-CREATE TABLE IF NOT EXISTS member (
-    member_id SERIAL NOT NULL,
-    email VARCHAR(45) NULL,
-    password VARCHAR(100) NULL,
-    PRIMARY KEY(member_id)
+CREATE TABLE IF NOT EXISTS "user" (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(45),
+    nickname VARCHAR(45),
+    introduction VARCHAR(45),
+    user_lock BOOLEAN,
+    password VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS block (
+    id SERIAL PRIMARY KEY,
+    block_lock BOOLEAN,
+    save BOOLEAN,
+    title VARCHAR(45),
+    block_color VARCHAR(10),
+    date DATE,
+    emotion VARCHAR(45),
+    user_id INTEGER REFERENCES "user"
+);
+
+CREATE TABLE IF NOT EXISTS task (
+    id SERIAL PRIMARY KEY,
+    task VARCHAR(100),
+    status BOOLEAN,
+    block_id INTEGER REFERENCES block
+
 );
