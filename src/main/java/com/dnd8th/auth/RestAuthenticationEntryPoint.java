19af28a4642478a5c627da89aaa46b41ef76ad1a
pasly0920 <inwoo920621@gmail.com>
2023-02-19 01:38:05 +0900

    feat: 인증 인가되지 않은 요청 발생시 진입점

--- src/main/java/com/dnd8th/auth/RestAuthenticationEntryPoint.java
+++ src/main/java/com/dnd8th/auth/RestAuthenticationEntryPoint.java
@@ -1,9 +1,13 @@
 package com.dnd8th.auth;
 
+import com.dnd8th.error.ErrorResponse;
+import com.dnd8th.error.exception.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -12,6 +16,11 @@ public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
-        response.setStatus(401);
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        response.setStatus(ErrorCode.USER_ACCESS_DENIED.getStatus());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(objectMapper.writeValueAsString(
+                ErrorResponse.from(ErrorCode.USER_ACCESS_DENIED)));
     }
 }
