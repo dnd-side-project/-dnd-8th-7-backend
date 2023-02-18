24daa983f63f70bf485fd592cb18f7049a73ea49
pasly0920 <inwoo920621@gmail.com>
2023-02-18 18:03:31 +0900

    fix: AccessTokenNotFoundException error code 지정

--- src/main/java/com/dnd8th/error/exception/auth/AccessTokenNotFoundException.java
+++ src/main/java/com/dnd8th/error/exception/auth/AccessTokenNotFoundException.java
@@ -5,7 +5,7 @@ import com.dnd8th.error.exception.ErrorCode;
 
 public class AccessTokenNotFoundException extends AccessDeniedException {
 
-    public AccessTokenNotFoundException(ErrorCode errorCode) {
-        super(errorCode);
+    public AccessTokenNotFoundException() {
+        super(ErrorCode.ACCESS_TOKEN_NOT_FOUND_IN_HEADER);
     }
 }
