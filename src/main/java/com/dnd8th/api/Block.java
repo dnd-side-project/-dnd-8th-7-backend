4272ab949929b9c13b49a4c3e27b7aa9aa66a3bc
rineeee <harin14@naver.com>
2023-02-19 00:14:07 +0900

    feat: add main api

---
+++ src/main/java/com/dnd8th/api/Block.java
@@ -0,0 +1,27 @@
+package com.dnd8th.api;
+
+
+import com.dnd8th.dto.MainWeekDTO;
+import com.dnd8th.service.BlockService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@Slf4j
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class Block {
+
+    private final BlockService blockService;
+
+    @GetMapping("/api/main/block/{date}")
+    public MainWeekDTO findUser(@PathVariable("date") String date) throws ParseException {
+        return blockService.getBlockWeek("1",date);
+    }
+}
