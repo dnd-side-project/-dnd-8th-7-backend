3fe4aa9e3db82e3301d6b3b4dc5e35e7a80d2345
rineeee <harin14@naver.com>
2023-02-19 20:32:52 +0900

    feat: dao 추가

--- src/main/java/com/dnd8th/dao/BlockFindDao.java
+++ src/main/java/com/dnd8th/dao/BlockFindDao.java
@@ -1,7 +1,13 @@
 package com.dnd8th.dao;
 
+import com.dnd8th.dto.BlockDTO;
+import com.dnd8th.dto.TaskDTO;
+import com.dnd8th.entity.Block;
+import com.dnd8th.entity.Task;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,9 +15,11 @@ import java.util.Date;
 import java.util.List;
 
 import static com.dnd8th.entity.QBlock.block;
+import static com.dnd8th.entity.QTask.task;
 
 
 @Service
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class BlockFindDao{
@@ -21,6 +29,22 @@ public class BlockFindDao{
         return queryFactory.select(block.blockColor)
                 .from(block)
                 .where(block.user.email.eq(email), block.date.eq(date))
+                .fetch();
+    }
+
+    public List<Block> getDailyBlock(String email, Date date) {
+        return queryFactory
+                .select(block)
+                .from(block)
+                .where(block.user.email.eq(email), block.date.eq(date))
+                .fetch();
+    }
+
+    public List<Task> getDailyTask(long bockId) {
+        return queryFactory
+                .select(task)
+                .from(task)
+                .where(task.block.id.eq(bockId))
                 .fetch();
     }
 }
