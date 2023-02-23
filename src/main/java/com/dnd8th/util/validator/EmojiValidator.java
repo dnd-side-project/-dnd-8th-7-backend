e7b0499bd573af4235c7971ba2b18d954122317f
rineeee <harin14@naver.com>
2023-02-23 19:45:41 +0900

    feat: 이모지 판별

--- src/main/java/com/dnd8th/util/validator/EmojiValidator.java
+++ src/main/java/com/dnd8th/util/validator/EmojiValidator.java
@@ -2,11 +2,12 @@ package com.dnd8th.util.validator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import com.vdurmont.emoji.EmojiManager;
 
 public class EmojiValidator implements ConstraintValidator<Emoji, String> {
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value.matches("[^\\p{L}]+");
+        return EmojiManager.isEmoji(value);
     }
 }
