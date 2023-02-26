05e5464ddeffce87e0f37f36df621a04989c1448
pasly0920 <inwoo920621@gmail.com>
2023-02-26 09:31:50 +0900

    feat: task crud 소유권 검증 로직 추가

--- src/main/java/com/dnd8th/api/TaskApi.java
+++ src/main/java/com/dnd8th/api/TaskApi.java
@@ -4,14 +4,21 @@ import com.dnd8th.dto.task.TaskCreateRequest;
 import com.dnd8th.dto.task.TaskCreateResponse;
 import com.dnd8th.dto.task.TaskUpdateRequest;
 import com.dnd8th.entity.Task;
-import javax.validation.Valid;
-
 import com.dnd8th.service.TaskService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -26,32 +33,39 @@ public class TaskApi {
             @RequestBody @Valid TaskCreateRequest taskCreateRequest,
             @PathVariable("blockId") Long blockId) {
         Task newTask = taskService.createTask(taskCreateRequest, blockId);
-        TaskCreateResponse taskCreateResponse = new TaskCreateResponse(newTask.getId(), newTask.getContents());
+        TaskCreateResponse taskCreateResponse = new TaskCreateResponse(newTask.getId(),
+                newTask.getContents());
         return ResponseEntity.status(HttpStatus.CREATED).body(taskCreateResponse);
     }
 
     @DeleteMapping("/{taskId}")
     public ResponseEntity<String> deleteTask(
+            @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable("taskId") Long taskId
-            ) {
-        taskService.deleteTask(taskId);
+    ) {
+        String email = userDetails.getUsername();
+        taskService.deleteTask(email, taskId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
 
     @PatchMapping("/{taskId}")
     public ResponseEntity<String> updateTask(
+            @AuthenticationPrincipal UserDetails userDetails,
             @RequestBody @Valid TaskUpdateRequest taskUpdateRequest,
             @PathVariable("taskId") Long taskId
     ) {
-        taskService.updateTask(taskId, taskUpdateRequest.getContent());
+        String email = userDetails.getUsername();
+        taskService.updateTask(email, taskId, taskUpdateRequest.getContent());
         return ResponseEntity.status(HttpStatus.OK).body("");
     }
 
     @PatchMapping("/status/{taskId}")
     public ResponseEntity<String> updateStatus(
+            @AuthenticationPrincipal UserDetails userDetails,
             @PathVariable("taskId") Long taskId
     ) {
-        taskService.toggleStatus(taskId);
+        String email = userDetails.getUsername();
+        taskService.toggleStatus(email, taskId);
         return ResponseEntity.status(HttpStatus.OK).body("");
     }
 }
