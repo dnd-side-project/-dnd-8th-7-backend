713c9126e51e360a81b6e1355f7320496ffbd084
pasly0920 <inwoo920621@gmail.com>
2023-03-03 23:08:39 +0900

    feat: username duplicated dao 추가

---
+++ src/main/java/com/dnd8th/dao/UserNameFindDuplicatedDao.java
@@ -0,0 +1,25 @@
+package com.dnd8th.dao;
+
+import static com.dnd8th.entity.QUser.user;
+
+import com.dnd8th.entity.User;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserNameFindDuplicatedDao {
+
+    private final JPAQueryFactory queryFactory;
+
+    public boolean isNameDuplicated(String nickname) {
+        User findUser = queryFactory.selectFrom(user)
+                .where(user.name.eq(nickname))
+                .fetchOne();
+
+        return (findUser != null);
+    }
+}
