d3c71008205480e2c361f2f0484ae5e6494795f1
pasly0920 <inwoo920621@gmail.com>
2023-03-04 00:50:27 +0900

    fix: task content empty 허용

--- src/main/java/com/dnd8th/dto/task/TaskUpdateRequest.java
+++ src/main/java/com/dnd8th/dto/task/TaskUpdateRequest.java
@@ -4,12 +4,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @NoArgsConstructor
 public class TaskUpdateRequest {
-    @NotBlank
+
     private String content;
 
     @Builder
