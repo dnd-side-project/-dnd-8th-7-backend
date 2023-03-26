13506984532035a41f04bbc1bbb855b2fce287b1
pasly0920 <inwoo920621@gmail.com>
2023-03-26 19:41:11 +0900

    fix: fetch lazy 누락 부분 추가

--- src/main/java/com/dnd8th/entity/Block.java
+++ src/main/java/com/dnd8th/entity/Block.java
@@ -3,8 +3,18 @@ package com.dnd8th.entity;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.*;
-
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -44,7 +54,7 @@ public class Block extends BaseEntity {
     @JoinColumn(name = "user_email")
     private User user;
 
-    @OneToOne(mappedBy = "block", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @OneToOne(mappedBy = "block", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
     private Keep keep;
 
     @Builder
--- src/main/java/com/dnd8th/entity/Keep.java
+++ src/main/java/com/dnd8th/entity/Keep.java
@@ -1,16 +1,25 @@
 package com.dnd8th.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
 @Table(name = "keep")
 @NoArgsConstructor
 public class Keep extends BaseEntity {
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,7 +29,7 @@ public class Keep extends BaseEntity {
     @JoinColumn(name = "user_email")
     private User user;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "block_id")
     private Block block;
 
@@ -37,7 +46,8 @@ public class Keep extends BaseEntity {
     private Integer sumOfTask;
 
     @Builder
-    public Keep(User user, Block block, String title, String blockColor, String emotion, Integer sumOfTask) {
+    public Keep(User user, Block block, String title, String blockColor, String emotion,
+            Integer sumOfTask) {
         this.user = user;
         this.block = block;
         this.title = title;
