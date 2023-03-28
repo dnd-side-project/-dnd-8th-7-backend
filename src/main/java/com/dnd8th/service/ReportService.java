e601a7b118e0ecdfcf3cde9e29531f0d424781e5
pasly0920 <inwoo920621@gmail.com>
2023-03-28 15:56:38 +0900

    feat: 최저 달성률 블록 API 추가 및 기존 코드 리팩토링

--- src/main/java/com/dnd8th/service/ReportService.java
+++ src/main/java/com/dnd8th/service/ReportService.java
@@ -30,54 +30,8 @@ public class ReportService {
         }
     }
 
-    public ReportBlockGetResponse getMostTaskRateBlock(String userEmail, Integer month) {
-        isMonthValid(month);
-
-        List<MonthlyBlockAndTaskGetDTO> monthlyBlock = getMonthlyBlockAndTask(userEmail, month);
-        if (monthlyBlock.isEmpty()) {
-            return new ReportBlockGetResponse(null);
-        }
-
-        String mostTaskRateBlockContent = getMostTaskRateBlockContent(monthlyBlock);
-
-        return new ReportBlockGetResponse(mostTaskRateBlockContent);
-    }
-
-    public ReportBlockGetResponse getMostMadeBlock(String userEmail, Integer month) {
-        isMonthValid(month);
-
-        List<MonthlyBlockGetDTO> monthlyBlock = getMonthlyBlock(userEmail, month);
-
-        //business logic
-        if (monthlyBlock.isEmpty()) {
-            return new ReportBlockGetResponse(null);
-        }
-
-        String mostMadeBlockContent = getMostMadeBlockContent(monthlyBlock);
-        return new ReportBlockGetResponse(mostMadeBlockContent);
-    }
-
-    private String getMostMadeBlockContent(List<MonthlyBlockGetDTO> monthlyBlock) {
-        List<String> blockContentList = monthlyBlock.stream().map(MonthlyBlockGetDTO::getTitle)
-                .collect(Collectors.toList());
-
-        Map<String, Integer> map = new HashMap<>();
-        for (String s : blockContentList) {
-            map.merge(s, 1, Integer::sum);
-        }
-
-        return map.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .orElse(null);
-    }
-
-    private List<MonthlyBlockGetDTO> getMonthlyBlock(String userEmail, Integer month) {
-        return monthlyBlockGetDao.getMonthlyBlockList(userEmail, month);
-    }
-
-    private String getMostTaskRateBlockContent(List<MonthlyBlockAndTaskGetDTO> monthlyBlock) {
-        //hash map key: content string, value: task achieved rate
+    private static Map<String, List<Double>> getContentAchieveRateMap(
+            List<MonthlyBlockAndTaskGetDTO> monthlyBlock) {
         Map<String, List<Double>> blockTaskRateMap = new HashMap<>();
         for (MonthlyBlockAndTaskGetDTO block : monthlyBlock) {
             List<MonthlyTaskGetDTO> tasks = block.getTasks();
@@ -102,6 +56,91 @@ public class ReportService {
 
             blockTaskRateMap.put(blockContent, rates);
         }
+        return blockTaskRateMap;
+    }
+
+    public ReportBlockGetResponse getMostTaskRateBlock(String userEmail, Integer month) {
+        isMonthValid(month);
+
+        List<MonthlyBlockAndTaskGetDTO> monthlyBlock = getMonthlyBlockAndTask(userEmail, month);
+        if (monthlyBlock.isEmpty()) {
+            return new ReportBlockGetResponse(null);
+        }
+
+        String mostTaskRateBlockContent = getMostTaskRateBlockContent(monthlyBlock);
+        return new ReportBlockGetResponse(mostTaskRateBlockContent);
+    }
+
+    public ReportBlockGetResponse getMostMadeBlock(String userEmail, Integer month) {
+        isMonthValid(month);
+
+        List<MonthlyBlockGetDTO> monthlyBlock = getMonthlyBlock(userEmail, month);
+        if (monthlyBlock.isEmpty()) {
+            return new ReportBlockGetResponse(null);
+        }
+
+        String mostMadeBlockContent = getMostMadeBlockContent(monthlyBlock);
+        return new ReportBlockGetResponse(mostMadeBlockContent);
+    }
+
+    public ReportBlockGetResponse getLeastTaskRateBlock(String userEmail, Integer month) {
+        isMonthValid(month);
+
+        List<MonthlyBlockAndTaskGetDTO> monthlyBlock = getMonthlyBlockAndTask(userEmail, month);
+        if (monthlyBlock.isEmpty()) {
+            return new ReportBlockGetResponse(null);
+        }
+
+        String leastTaskRateBlockContent = getLeastTaskRateBlockContent(monthlyBlock);
+        return new ReportBlockGetResponse(leastTaskRateBlockContent);
+    }
+
+    private String getLeastTaskRateBlockContent(List<MonthlyBlockAndTaskGetDTO> monthlyBlock) {
+        //hash map key: content string, value: task achieved rate
+        Map<String, List<Double>> blockTaskRateMap = getContentAchieveRateMap(monthlyBlock);
+
+        //one more loop and get the highest value(task achieved rate)
+        String leastTaskRateBlockContent = null;
+        Double lowestAvgRate = 1.0;
+
+        for (Map.Entry<String, List<Double>> entry : blockTaskRateMap.entrySet()) {
+            List<Double> rates = entry.getValue();
+
+            Double avgRate = rates.stream().mapToDouble(rate -> rate).average().orElse(0.0);
+
+            if (avgRate < lowestAvgRate) {
+                lowestAvgRate = avgRate;
+                leastTaskRateBlockContent = entry.getKey();
+            }
+        }
+
+        //return the key(content string)
+        return leastTaskRateBlockContent;
+    }
+
+    private String getMostMadeBlockContent(List<MonthlyBlockGetDTO> monthlyBlock) {
+        List<String> blockContentList = monthlyBlock.stream().map(MonthlyBlockGetDTO::getTitle)
+                .collect(Collectors.toList());
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : blockContentList) {
+            map.merge(s, 1, Integer::sum);
+        }
+
+        return map.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
+
+    private List<MonthlyBlockGetDTO> getMonthlyBlock(String userEmail, Integer month) {
+        return monthlyBlockGetDao.getMonthlyBlockList(userEmail, month);
+    }
+
+    private String getMostTaskRateBlockContent(List<MonthlyBlockAndTaskGetDTO> monthlyBlock) {
+        //hash map key: content string, value: task achieved rate
+        Map<String, List<Double>> blockTaskRateMap = getContentAchieveRateMap(
+                monthlyBlock);
 
         //one more loop and get the highest value(task achieved rate)
         String mostTaskRateBlockContent = null;
@@ -126,5 +165,4 @@ public class ReportService {
             Integer month) {
         return monthlyBlockTaskGetDao.getMonthlyBlockAndTask(userEmail, month);
     }
-
 }
