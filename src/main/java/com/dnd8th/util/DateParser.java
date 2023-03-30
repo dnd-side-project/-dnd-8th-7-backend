20772b12cbb4602d42a3d4b06263884d0ffb2b80
pasly0920 <inwoo920621@gmail.com>
2023-03-30 11:16:59 +0900

    feat: api parameter date parser util 함수 추가

--- src/main/java/com/dnd8th/util/DateParser.java
+++ src/main/java/com/dnd8th/util/DateParser.java
@@ -1,15 +1,23 @@
 package com.dnd8th.util;
 
 import com.dnd8th.error.exception.block.DateFormatInvalidException;
-import org.springframework.stereotype.Component;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import org.springframework.stereotype.Component;
 
 @Component
 public class DateParser {
+
     private static SimpleDateFormat formatting = new SimpleDateFormat("yyyy-MM-dd");
+
+    public Date getDate(int year, int month, int date) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, date, 0, 0, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return new Date(cal.getTimeInMillis());
+    }
 
     public Date parseDate(String date) {
         try {
