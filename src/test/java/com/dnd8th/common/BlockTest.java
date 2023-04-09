dedb95850b94b31ae84c09ef0aa1407334217e49
pasly0920 <inwoo920621@gmail.com>
2023-04-09 15:54:26 +0900

    test: block 관련 Test 추가

--- src/test/java/com/dnd8th/common/BlockTest.java
+++ src/test/java/com/dnd8th/common/BlockTest.java
@@ -2,6 +2,7 @@ package com.dnd8th.common;
 
 import com.dnd8th.auth.jwt.JwtProviderService;
 import com.dnd8th.dao.block.BlockFindDao;
+import com.dnd8th.dao.block.BlockUpdateDao;
 import com.dnd8th.entity.Block;
 import com.dnd8th.entity.Role;
 import com.dnd8th.entity.User;
@@ -10,6 +11,7 @@ import com.dnd8th.repository.UserRepository;
 import com.dnd8th.service.BlockService;
 import com.dnd8th.util.DateParser;
 import io.restassured.RestAssured;
+import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 public abstract class BlockTest {
+
     @Autowired
     protected UserRepository userRepository;
     @Autowired
@@ -27,10 +30,15 @@ public abstract class BlockTest {
     protected JwtProviderService jwtProviderService;
     @Autowired
     protected BlockService blockService;
+
     @Autowired
     protected DateParser dateParser;
     @Autowired
     protected BlockFindDao blockFindDao;
+    @Autowired
+    protected BlockUpdateDao blockUpdateDao;
+    @Autowired
+    protected EntityManager entityManager;
 
     @LocalServerPort
     protected int port;
@@ -45,6 +53,13 @@ public abstract class BlockTest {
                 .imagePath("")
                 .build()
         );
+
+        User testUser2 = userRepository.save(User.builder()
+                .name("test2")
+                .email("test2@gmail.com")
+                .role(Role.USER)
+                .imagePath("")
+                .build());
 
         for (int i = 1; i < 4; i++) {
             blockRepository.save(Block.builder()
